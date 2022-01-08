9eb653fe3357474e01c30cbc13075d0ee7082ce7
OshadhaShiro <shirokumara49@gmail.com>
2022-01-08 21:04:00 +0530

    Realtime chat func complete

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 	return (
 		<div className="h-screen overflow-y-scroll bg-gradient-to-b from-black to-fuchsia-900 overflow-hidden">
 			<Head>
-				<title> Metaverse Challenge </title>
+				<title> TECHforGEEKS </title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
