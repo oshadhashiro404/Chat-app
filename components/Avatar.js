0333668597fef0eda9d1a13f69227abcedaeabb3
OshadhaShiro <shirokumara49@gmail.com>
2022-01-08 20:57:34 +0530

    Realtime chat func complete

---
+++ components/Avatar.js
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import { useMoralis } from 'react-moralis';
+
+function Avatar({ username, logoutOnPress }) {
+	const { user, logout } = useMoralis();
+
+	return (
+		<Image
+			className={`rounded-full bg-black  ${
+				logoutOnPress && 'cursor-pointer hover:opacity-75'
+			}`}
+			src={`https://avatars.dicebear.com/api/pixel-art/${
+				username || user.get('username')
+			}.svg`}
+			onClick={() => logoutOnPress && logout()}
+			layout="fill"
+		/>
+	);
+}
+
+export default Avatar;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import Login from '../Components/Login';
 import { useMoralis } from 'react-moralis';
+import Header from '../components/Header';
+import Messages from '../components/Messages';
 
 export default function Home() {
 	const { isAuthenticated, /*isInitializing,*/ logout } = useMoralis();
@@ -8,13 +10,16 @@ export default function Home() {
 	// if (isInitializing) return 'Loading....';
 	if (!isAuthenticated) return <Login />;
 	return (
-		<div className="h-screen overflow-y-scroll overflow-hidden">
+		<div className="h-screen overflow-y-scroll bg-gradient-to-b from-black to-fuchsia-900 overflow-hidden">
 			<Head>
-				<title> Metaverse Challenge </title>{' '}
+				<title> Metaverse Challenge </title>
 				<link rel="icon" href="/favicon.ico" />
-			</Head>{' '}
-			<h1>Metaverse Challenge</h1>
-			<button onClick={logout}>Logout</button>
+			</Head>
+
+			<div className="max-w-screen-2xl mx-auto">
+				<Header />
+				<Messages />
+			</div>
 		</div>
 	);
 }
